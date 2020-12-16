b2b87c4559d834e37c575c8c348d2468722e4959
Luiza Campos <luiza.almcampos@gmail.com>
2020-12-16 17:29:44 -0300

    game

---
+++ README.md
@@ -0,0 +1,4 @@
+Jogo de pedra, papel e tesoura desenvolvido com JavaScript puro.
+a tela do jogo possui apenas um botão para iniciar o jogo, quando apertado uma caixa de diálogo se abre perguntando qual a escolha do jogador (rock, paper or scissors). 
+Em seguida o computador faz sua eacolha randomicamente e o resultado do jogo é exibido em uma caixa de alerta.
+Jogo desenvolvido com HTML5, CSS3 e JavaScript.
---
+++ app.js
@@ -0,0 +1,75 @@
+const startGameBtn = document.getElementById('start-game-btn');
+
+const ROCK = 'ROCK'
+const PAPER = 'PAPER'
+const SCISSORS = 'SCISSORS'
+const DEFAULT_USER_CHOICE = ROCK
+const RESULT_DRAW = 'DRAW'
+const RESULT_PLAYER_WINS = 'PLAYER_WINS'
+const RESULT_COMPUTER_WINS = 'COMPUTER_WINS'
+
+let gameIsRunning = false
+
+
+const getPlayerChoice = function() {
+    const selection = prompt(`${ROCK}, ${PAPER} or ${SCISSORS}?`, '').toUpperCase()
+    if (
+        selection !== ROCK &&
+        selection !== PAPER &&
+        selection !== SCISSORS
+    ) {
+        alert(`Invalid choice! We chose ${DEFAULT_USER_CHOICE} for you!`)
+        return
+    } 
+    return selection
+}
+
+const getComputerChoice = function() {
+    const randomValue = Math.random()
+    if (randomValue < 0.34) {
+        return ROCK
+    } else if (randomValue < 0.67) {
+        return PAPER 
+    } else {
+        return SCISSORS
+    }
+}
+ 
+const getWinner = (cChoice, pChoice = DEFAULT_USER_CHOICE) => {
+    if (cChoice === pChoice) {
+        return RESULT_DRAW
+    } else if (
+        cChoice === ROCK && pChoice === PAPER || 
+        cChoice === PAPER && pChoice === SCISSORS ||
+        cChoice === SCISSORS && pChoice === ROCK
+    ) {
+        return RESULT_PLAYER_WINS
+    } else {
+        return RESULT_COMPUTER_WINS
+    }
+}
+
+startGameBtn.addEventListener('click', () => {
+    if (gameIsRunning) {
+        return
+    }
+    gameIsRunning = true
+    const playerChoice = getPlayerChoice()
+    const computerChoice = getComputerChoice()
+    let winner
+    if (playerChoice) {
+        winner = getWinner(computerChoice, playerChoice)
+    } else {
+        winner = getWinner(computerChoice)
+    }
+    let message = `You picked ${playerChoice ? playerChoice : DEFAULT_USER_CHOICE}, computer picked ${computerChoice}. `
+    if (winner === RESULT_DRAW) {
+        message = message + "It's a DRAW!"
+    } else if (winner === RESULT_COMPUTER_WINS) {
+        message = message + 'You LOST!!'
+    } else {
+        message = message + 'You WON!!'
+    }
+    alert(message)
+    gameIsRunning = false
+})
